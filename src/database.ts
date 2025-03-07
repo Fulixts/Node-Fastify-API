2c1d5cb8a8da031b17307ae015653aed70f732e5
Fulixts <gabriel_fuli308@hotmail.com>
2025-03-07 16:42:32 -0300

    fix: adjusting port to show on log

--- src/database.ts
+++ src/database.ts
@@ -4,6 +4,7 @@ import { env } from './env'
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL env not found.')
 }
+
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
   connection:
@@ -12,6 +13,7 @@ export const config: Knex.Config = {
           filename: env.DATABASE_URL,
         }
       : env.DATABASE_URL,
+
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
